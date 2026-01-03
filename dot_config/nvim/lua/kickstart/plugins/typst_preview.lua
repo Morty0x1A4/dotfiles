ac519927623f6a65ecc9508cd47e2f9b5cb30518
Morty0x1A4 <42069a@gmx.net>
2026-01-03 11:47:11 +0100

    Added fish, niri, waybar,swaylock

---
+++ dot_config/nvim/lua/kickstart/plugins/typst_preview.lua
@@ -0,0 +1,8 @@
+return {
+  'chomosuke/typst-preview.nvim',
+  lazy = false, -- or ft = 'typst'
+  invert_colors = 'always',
+  follow_cursor = true,
+  version = '1.*',
+  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+}
